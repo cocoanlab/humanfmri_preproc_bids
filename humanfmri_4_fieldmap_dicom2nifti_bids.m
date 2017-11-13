337b213fd1223fc1b10c3185fd9c92e906056cfe
wanirepo <choongwan.woo@gmail.com>
2017-11-13 19:15:12 +0900

    updates

--- humanfmri_4_fieldmap_dicom2nifti_bids.m
+++ humanfmri_4_fieldmap_dicom2nifti_bids.m
@@ -74,7 +74,7 @@ f = fields(out.h);
 nifti_3d = filenames([f{2} '*.nii']);
 
 [~, subj_id] = fileparts(PREPROC.subject_dir);
-output_4d_fnames = fullfile(outdir, sprintf('%s_dir_ap_epi', subj_id));
+output_4d_fnames = fullfile(outdir, sprintf('%s_dir-ap_epi', subj_id));
     
 disp('Converting 3d images to 4d images...')
 spm_file_merge(nifti_3d, [output_4d_fnames '.nii']);
