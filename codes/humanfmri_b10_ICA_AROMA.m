c82e10c9fbfbbf3ea5177bb6c49b7a0fb5a7dcba
wanirepo <choongwan.woo@gmail.com>
2018-06-20 15:30:22 +0900

    resample ICA-AROMA mask space if the space is different

--- codes/humanfmri_b10_ICA_AROMA.m
+++ codes/humanfmri_b10_ICA_AROMA.m
@@ -44,7 +44,7 @@ function PREPROC = humanfmri_b10_ICA_AROMA(preproc_subject_dir, varargin)
 % ..
 
 % you can change the default for your computer
-ica_aroma_dir = '/Users/clinpsywoo/github/ICA-AROMA';
+ica_aroma_dir = '/Users/clinpsywoo/Dropbox/github/ICA-AROMA';
 anaconda_dir = '/Users/clinpsywoo/anaconda/bin';
 
 for i = 1:numel(varargin)
@@ -74,6 +74,27 @@ addpath(genpath(anaconda_dir))
 
 ica_aroma = fullfile(ica_aroma_dir, 'ICA_AROMA.py');
 
+% compare the space bewteen mask and data and resample if necessary
+subject_dir = preproc_subject_dir{1};
+PREPROC = save_load_PREPROC(subject_dir, 'load'); % load PREPROC
+current_data = fmri_data([PREPROC.swr_func_bold_files{1} ',1']);
+ica_mask{1} = fmri_data(fullfile(ica_aroma_dir, 'mask_csf.nii.gz'));
+
+if compare_space(current_data, ica_mask{1})
+    ica_mask{2} = fmri_data(fullfile(ica_aroma_dir, 'mask_edge.nii.gz'));
+    ica_mask{3} = fmri_data(fullfile(ica_aroma_dir, 'mask_out.nii.gz'));
+    
+    for i = 1:numel(ica_mask)
+        ica_mask{i} = resample_space(ica_mask{i}, current_data, 'nearest');
+        ica_mask{i}.fullpath = fullfile(ica_aroma_dir, ica_mask{i}.image_names);
+        write(ica_mask{i});
+    end
+    
+    gz_mask_files = filenames(fullfile(ica_aroma_dir, '*nii.gz'));
+    for i = 1:numel(gz_mask_files), delete(gz_mask_files{i}); end
+    system(['gzip ' fullfile(ica_aroma_dir, '*.nii')]);
+end
+
 for subj_i = 1:numel(preproc_subject_dir)
 
     subject_dir = preproc_subject_dir{subj_i};
@@ -86,7 +107,7 @@ for subj_i = 1:numel(preproc_subject_dir)
         
         [d, f] = fileparts(PREPROC.swr_func_bold_files{run_i});
         mvmt_fname = fullfile(d, ['mvmt_' f '.txt']);
-        dlmwrite(mvmt_fname, PREPROC.nuisance.mvmt_covariates{run_i});
+        dlmwrite(mvmt_fname, PREPROC.nuisance.mvmt_covariates{run_i}, 'delimiter','\t');
 
         system([anaconda_dir '/python2.7 ' ica_aroma ' -in ' PREPROC.swr_func_bold_files{run_i} ' -out ' PREPROC.preproc_func_dir ' -mc ' mvmt_fname ' -tr ' num2str(PREPROC.TR)]);
     end
