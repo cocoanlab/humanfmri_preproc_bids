186aa3ecfcf0e6f3d6269089cf612f0b8f71f70c
Suhwan Gim <roseno.9@daum.net>
2019-02-08 14:29:14 +0900

    add new functions for making nuisance matrix and calculating framewise displacement

---
+++ codes/humanfmri_c2_get_framewise_displacement.m
@@ -0,0 +1,216 @@
+function PREPROC = humanfmri_c2_get_framewise_displacement(preproc_subject_dir,varargin)
+
+% Compute framewise displacement and apply exculsion criteria and save
+% plots in PREPROC.qcdir
+%
+% :Usage:
+% ::
+%
+%    [results, FD, MeanFd] = getFD(preproc_subject_dir,varargin)
+%
+% :Inputs:
+%
+%   - preproc_subject_dir: the subject directory for preprocessed data
+%                             (PREPROC.preproc_outputdir)
+%
+% :Optional Inputs:
+%
+%   **'type':**
+%       : specify type of FD (default: 'Power's FD')
+%
+% :Output:
+%
+%   PREPROC.framewise_displacement
+%   save plots in PREPROC.qcdir
+%
+% Copyright (C) 2018  Suhwan Gim
+%
+% ..
+%
+% :For example
+%1) humanfmri_c2_get_framewise_displacement(preproc_subject_dir,'type','VD')
+%2) humanfmri_c2_get_framewise_displacement(preproc_subject_dir) %default
+%% functional commands
+save_plot = ture;
+FD_id = 'Power';
+FdJenkThr = 0.25;
+for i = 1:length(varargin)
+    if ischar(varargin{i})
+        switch varargin{i}
+            % functional commands
+            case {'type'}
+                FD_id = varargin{i+1};
+        end
+    end
+end
+%%
+for subj_i = 1:numel(preproc_subject_dir)
+    % load PREPROC and Print header
+    subject_dir = preproc_subject_dir{subj_i};    
+    PREPROC = save_load_PREPROC(subject_dir, 'load'); % load PREPROC
+    print_header(['Framewise Displacement: ' FD_id], PREPROC.subject_code);       
+    %% Set Movement data (PREPROC.nuisance.mvmt_covariates)
+    for run_i = 1:numel(PREPROC.nuisance.mvmt_covariates)
+        MoveParams{run_i} = [PREPROC.nuisance.mvmt_covariates{run_i}];
+        SpCov{run_i} = [PREPROC.nuisance.spike_covariates{run_i}]; %spikie covariates
+    end
+    %% Get FD
+    switch FD_id
+        case {'Power'} % Power's FD
+            fprintf(1,'Computing Power''s framewise displacement...\n');
+            for run_i = 1:length(MoveParams)
+                % ======================================================================= %
+                % mov       - an N x 6 matrix containing 6 movement parameters and where N
+                %           = length of the time series.
+                % head      - head radius (in mm) to use when converting radians to mm.
+                % 			default = 50mm, as in Power et al.
+                % ======================================================================= %
+                head = 50; %maybe default?
+                
+                % detrend motion regressors
+                
+                mov = MoveParams{run_i}; % x,y,z
+                mov = detrend(mov,'linear');
+                % convert degrees to radians to mm
+                mov(:,4:6) = head*pi/180*mov(:,4:6);
+                
+                % differentiate movement parameters
+                delta_mov = [
+                    zeros(1,size(mov,2));
+                    diff(mov);
+                    ];
+                
+                % compute total framewise displacement
+                FD{run_i} = sum(abs(delta_mov),2);
+                MeanFd(run_i) = mean(FD{run_i});
+            end
+            
+        case {'VD'} % Van Dijk's FD
+            fprintf(1,'Computing Van Dijk''s framewise displacement...\n');
+            for run_i = 1:length(MoveParams)
+                mov = MoveParams{run_i}(:,1:3); % x,y,z
+                % detrend motion regressors
+                mov = detrend(mov,'linear');
+                
+                % number of time points
+                N = size(mov,1);
+                
+                % initialise fd variable.
+                FD{run_i} = zeros(N,1);
+                
+                % start at volume 2
+                for ii = 2:N
+                    x = mov(ii,1);
+                    y = mov(ii,2);
+                    z = mov(ii,3);
+                    
+                    x_1 = mov(ii-1,1);
+                    y_1 = mov(ii-1,2);
+                    z_1 = mov(ii-1,3);
+                    
+                    FD{run_i}(ii) = rms([x y z]) - rms([x_1 y_1 z_1]);
+                    MeanFd(run_i) = mean(FD{run_i});
+                end
+            end
+        case {'Jenk'}
+            % not implemented yet
+            warning('Check your input, especially, FD''s name (VD, Power)');
+        otherwise
+            warning('Check your input, especially, FD''s name (VD, Power)');
+    end
+    
+    
+    
+    %% Exculsion criteria
+    
+    excul_1 = zeros(1,length(MoveParams));
+    excul_2 = zeros(1,length(MoveParams));
+    % 1) Intial criteria
+    excul_1(MeanFd > 0.55) = 1;
+    % 2) More stringent
+    % 2.1) greater than 0.2 mm (Ciric)
+    excul_2(MeanFd > 0.2) = 1;
+    for run_i = 1:length(FD)
+        % 2.2) Propotion ( > 20%)
+        if strcmp(FD_id,'Jenk')
+            FdTher = round(size(FD{run_i},1) * 0.20);
+            if sum(FD{run_i}> FdJenkThr) > FdTher
+                excul_2(run_i) = 1;
+            end
+        end
+        % 2.3) >5mm
+        if any(FD{run_i} > 5)
+            excul_2(run_i) = 1;
+        end
+    end
+    %% plot FD
+    c=1;
+    if save_plot
+        close all;
+        Nrow_subplot = length(FD);
+        for run_i=1:length(FD)
+            % draw plot
+            set(gcf, 'position', [1           1        1920         1000]);
+            subplot(Nrow_subplot,3,[c c+1]);
+            plot(FD{run_i});
+            % change title color if exceed exclusion criteria
+            if excul_1(run_i) | excul_2(run_i)
+                title(sprintf('run %02d',run_i),'color',[1 0 0]);
+            else
+                title(sprintf('run %02d',run_i));
+            end
+            
+            set(gca,'xlim', [0 length(FD{run_i})] ,'ylim',[0 0.7]);
+            
+            % draw histogram
+            subplot(Nrow_subplot,3,c+2);
+            
+            histogram(FD{run_i},70);
+            set(gca,'xlim', [0 0.7]);
+            
+            c=c+3;            
+            if excul_1(run_i) | excul_2(run_i)
+                title(['Mean: ' num2str(MeanFd(run_i))],'color',[1 0 0]);
+            else
+                title(['Mean: ' num2str(MeanFd(run_i))]);
+            end
+            % 0.2> 0.15 > 0.10
+            %         if MeanFd(run_i) > 0.2
+            %             title(['Mean: ' num2str(MeanFd(run_i))],'color',[1 0 0]);
+            %         elseif MeanFd(run_i) < 0.2 && MeanFd(run_i) > 0.15
+            %             title(['Mean: ' num2str(MeanFd(run_i))],'color',[1 0.2 0.2]);
+            %         elseif MeanFd(run_i) < 0.15 && MeanFd(run_i) > 0.10
+            %             title(['Mean: ' num2str(MeanFd(run_i))],'color',[1 0.4 0.4]);
+            %         else
+            %             title(['Mean: ' num2str(MeanFd(run_i))]);
+            %         end
+            
+            
+        end
+        set(gcf, 'color', 'w');
+        suptitle(char(['FrameWise Displacement: ' PREPROC.subject_code]));
+        figdir = PREPROC.qcdir;
+        graphwrite = fullfile(figdir, sprintf('FrameWise_Power_sub_%02d.png',sub_i));
+        pagesetup(gcf);
+        saveas(gcf,graphwrite);
+        pagesetup(gcf);
+        set(gcf, 'position', [1           1        1920         1000]);
+        saveas(gcf,graphwrite);
+    end
+    %% output
+    results.fd_id = FD_id;
+    results.FD = FD;
+    results.mean_FD = MeanFd;
+    results.raw = MoveParams;
+    results.exclusion_1 = excul_1;
+    results.exclusion_2 = excul_2;
+    results.exclusion_descrip = {...
+        'Description                                    : ''1'' means mark for exculsion; the order is order of run based on PREPROC'
+        'exclusion_1 (less stringent)                   : 1) mean FD > 0.55 mm '; ...
+        'exculsion_2 (more stringent and multi-criteria): 1) mean FD > 0.2mm, 2) (only Jenk''s) 3) large spike (5mm)'};
+    %% save PREPROC
+    PREPROC.framewise_displacement = results;
+    PREPROC = save_load_PREPROC(subject_dir, 'save'); % load PREPROC
+end
+
+end
---
+++ codes/humanfmri_c3_make_nuisance_regressors.m
@@ -0,0 +1,165 @@
+% ========================================================= %
+%                   Possible combinations                   %
+% --------------------------------------------------------- %
+% 1. 24 parameter + spike covatiates + linear drift         %
+% 2. 24 parameter + spike covariates + WM and CSF + linear  %
+% drfit                                                     %
+% 3. linear drift                                           %
+% ========================================================= %
+function humanfmri_c3_make_nuisance_regressors(preproc_subject_dir,varargin)
+
+% This funtion is for making and saving nuisance mat files using PREPROC
+% files.
+%
+% :Usage:
+%   [filename, fullpath] = make_nuisance_mat(PREPROC,varargin)
+%
+% :Input:
+% ::
+%   - preproc_subject_dir: the subject directory for preprocessed data
+%                             (PREPROC.preproc_outputdir)
+%
+% :Optional input
+% ::
+%   - 'regressors': parameter you want to include
+%                   (defaults: 'regressors','{'24Move','Spike','WM_CSF'};)
+%                   -> lists of regressors
+%                       1. {24Move}: 24 movement parameters
+%                       2. {Spike} : spike covariates
+%                       3. {WM_CSF}: WM and CSF
+%
+%   - 'img': if you want to estimate WM and CSF in specific imgs, you can
+%            specify field name in PREPROC struture
+%           (defaults: 'img','swr_func_bold_files' )
+%
+%
+% :Output:
+% ::
+%   - PREPROC.nuisacne_descriptions
+%   - PREPROC.nuisance_dir
+%   - PREPROC.nuisance_files
+%   - save 'nuisance mat files' in PREPROC.nuisacne_dir 
+%
+%
+% :Exmaples:
+%   - make_nuisance_regressors(PREPROC,'regressors',{'24Move','Spike','WM_CSF'})
+%   - make_nuisance_regressors(PREPROC,'img','swr_func_bold_files')
+%
+%
+% ..
+%     Author and copyright information:
+%
+%     Copyright (C) Jan 2019  Suhwan Gim
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+%
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+%
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% ..
+
+%% parse varagin
+do_24params = false;
+do_spike_covariates = false;
+do_wm_csf = false;
+reg_idx = {'24Move','Spike','WM_CSF'}; % defaults
+
+do_specify_img = false;
+fieldname = swr_func_bold_files;
+for i = 1:numel(varargin)
+    if ischar(varargin{i})
+        switch varargin{i}
+            case {'regressors'} % in seconds
+                reg_idx = varargin{i+1};
+            case {'img'}
+                fieldname = varargin{i+1};
+                do_specify_img = true;
+        end
+    end
+end
+%% 
+for subj_i = 1:numel(preproc_subject_dir)
+    % load PREPROC and Print header
+    subject_dir = preproc_subject_dir{subj_i};
+    PREPROC = save_load_PREPROC(subject_dir, 'load'); 
+    print_header('Make and save nuisance regressors: ', PREPROC.subject_code);
+    %% set the nuisance regressors
+    disp(':: List of nuisance regresosrs' )
+    for j = 1:length(reg_idx)
+        switch reg_idx{j}
+            case {'24Move','24MoveParams'}
+                do_24params = true;
+                disp('- 24 movement parameters');
+            case {'Spike','Spike_covariates'}
+                do_spike_covariates = true;
+                disp('- Spike covariates');
+            case {'WM_CSF','WMCSF','WhiteMatter_CSF'}
+                do_wm_csf = true;
+                disp('- White Matter and CSF');
+        end
+    end
+    disp('----------------------------------------------')
+    %% set directory
+    subj_dir = PREPROC.preproc_outputdir;
+    nuisance_dir = fullfile(subj_dir, 'nuisance_mat');
+    if ~exist(nuisance_dir, 'dir'), mkdir(nuisance_dir); end
+    %% make nuisance.mat
+    % warning('No nuisance files. Please check') input('');
+    for img_i = 1:numel(PREPROC.nuisance.mvmt_covariates)
+        R = [];
+        disp(['Run Number   : ' num2str(img_i)]);
+        disp('-------------------------------------------');
+        disp(['Nuisance file name: ', sprintf('nuisance_run%d.mat', img_i)]);
+        disp('-------------------------------------------');
+        % 1. 24 movement parameters
+        if do_24params
+            R = [[PREPROC.nuisance.mvmt_covariates{img_i} PREPROC.nuisance.mvmt_covariates{img_i}.^2 ...
+                [zeros(1,6); diff(PREPROC.nuisance.mvmt_covariates{img_i})] [zeros(1,6); diff(PREPROC.nuisance.mvmt_covariates{img_i})].^2]];
+        end
+        % 2. spike_covariates
+        if do_spike_covariates
+            R = [R  PREPROC.nuisance.spike_covariates{img_i}];
+        end
+        % 3. extract and add WM(value2)_CSF(value3)
+        if do_wm_csf
+            
+            if do_specify_img
+                eval(['images_by_run = PREPROC.' fieldname]);
+            else %defaults
+                images_by_run = PREPROC.swr_func_bold_files;
+            end
+            [~,img_name]=fileparts(images_by_run{img_i});
+            disp(['Img File name: ' img_name]);
+            [~, components] = extract_gray_white_csf(fmri_data(images_by_run{img_i}));
+            % but, see canlab_connectivity_preproc
+            R = [R double(components{2}) double(components{3})];
+        end
+        
+        % 4. finally, add linear drift
+        R = [R zscore((1:size(R,1))')];
+        
+        % 5. Save
+        savename{img_i} = fullfile(nuisance_dir, sprintf('nuisance_run%d.mat', img_i));
+        fprintf('\nsaving... %s\n\n', savename{img_i});
+        save(savename{img_i}, 'R');
+        
+    end
+    reg_idx{length(reg_idx)+1} = 'linear drift';
+    
+    % Save PROPROC
+    PREPROC.nuisacne_descriptions = reg_idx;
+    PREPROC.nuisance_dir = nuisance_dir;
+    PREPROC.nuisance_files = savename;
+    
+    save_load_PREPROC(subj_dir, 'save', PREPROC); % save PREPROC
+end
+
+end
+
