f97cebf0e06d7b4785d03f17ce5670609f7e7c5c
Hongjikim <redkim94@hanmail.net>
2019-03-07 21:49:24 +0900

    added options

--- codes/move_dicom_to_raw.m
+++ codes/move_dicom_to_raw.m
@@ -3,21 +3,21 @@ function move_dicom_to_raw(subject_code, study_imaging_dir, run_n)
 
 % The function move dicom files to corresponding directories in raw
 % folders.
-% YOU SHOULD CHECK YOUR DIRECTORY STRUCTURE OF DICOM FILES BEFORE USING THIS FUNCTION.                                        
-% This function is based on the directory structure of Cocoan lab. 
+% YOU SHOULD CHECK YOUR DIRECTORY STRUCTURE OF DICOM FILES BEFORE USING THIS FUNCTION.
+% This function is based on the directory structure of Cocoan lab.
 %
 % :Usage:
 % ::
 %    move_dicom_to_raw(subject_code, study_imaging_dir)
 %
 % :Inputs:
-% 
+%
 % - subject_code       the subject id
 %                      (e.g., subject_code = {'sub-caps001', 'sub-caps002'});
 % - study_imaging_dir  the directory information for the study imaging data
 %                      (e.g., study_imaging_dir = '/NAS/data/CAPS2/Imaging')
-% 
-% - run_n              number of runs: Previously decided in Setting. 
+%
+% - run_n              number of runs: Previously decided in Setting.
 %                      Equal to length of 'func_run_nums'
 %
 % ..
@@ -54,25 +54,38 @@ for sub_i = 1:numel(subject_codes)
     % anat
     raw_anat_dir = fullfile(raw_dir, 'dicom', 'anat');
     T1_dir = filenames(fullfile(sub_dicom_dir, ['T1*']), 'char');
-    movefile(T1_dir,raw_anat_dir);
+    if isempty(T1_dir)
+        input_key = input(' *** T1 directory is empty. Want to continue anyway? (c) Want to stop? (s):', 's');
+        if input_key == 's'
+            error('There is no T1 directory. Please check.')
+        end
+    else
+        movefile(T1_dir,raw_anat_dir);
+    end
     
     % fmap
     raw_fmap_dir = fullfile(raw_dir, 'dicom', 'fmap');
     DC_dir = filenames(fullfile(sub_dicom_dir, ['DISTORTION*']), 'char');
-    cd(raw_fmap_dir)
-    if size(DC_dir, 1) ==2
-        for dc_i = 1:2
-            movefile(deblank(DC_dir(dc_i,1:end)), raw_fmap_dir);
+    if isempty(DC_dir)
+        input_key = input(' *** Distortion Correction directory is empty. Want to continue anyway? (c) Want to stop? (s):', 's');
+        if input_key == 's'
+            error('There is no Distortion Correction directory. Please check.')
         end
     else
-        rep_num_dc = (size(DC_dir, 1)/2);
-        for dc_i = [rep_num_dc*1, rep_num_dc*2]
-            movefile(deblank(DC_dir(dc_i,1:end)), raw_fmap_dir)
+        if size(DC_dir, 1) ==2
+            for dc_i = 1:2
+                movefile(deblank(DC_dir(dc_i,1:end)), raw_fmap_dir);
+            end
+        else
+            rep_num_dc = (size(DC_dir, 1)/2);
+            for dc_i = [rep_num_dc*1, rep_num_dc*2]
+                movefile(deblank(DC_dir(dc_i,1:end)), raw_fmap_dir)
+            end
         end
     end
     
     % run1: FT1
-     for run_i = 1:run_n
+    for run_i = 1:run_n
         raw_run_dir = filenames(fullfile(raw_dir, 'dicom', ['*' num2str(run_i) '*']), 'char');
         dicom_run_dir = filenames(fullfile(sub_dicom_dir, [num2str(run_i), '*']), 'char');
         if size(dicom_run_dir, 1) == 2
@@ -92,5 +105,3 @@ for sub_i = 1:numel(subject_codes)
 end
 
 end
-
-
