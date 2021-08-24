43aa0e2e9b6fd0f7712aa454317a0f04b388704f
jaejoonglee92 <jaejoonglee92@gmail.com>
2021-08-24 17:20:54 +0900

    add options for slice timing correction

--- codes/humanfmri_b4_slice_timing.m
+++ codes/humanfmri_b4_slice_timing.m
@@ -20,11 +20,17 @@ function PREPROC = humanfmri_b4_slice_timing(preproc_subject_dir, tr, mbf, varar
 %
 % :Optional Input:
 %
-% - custom_slice_timing    Specification of slice timing. Order(integer
-%                          type) or timing (in milliseconds, float type).
+% - custom_slice_timing    Specification of slice timing.
+%                          Order(integer) or timing (in milliseconds, float)
+%                          or type (character) are available.
 %                          ex) Order : [1 3 5 7 2 4 6 8]',
 %                              Timing : [0.0000 252.5000 62.5000 315.0000
 %                                       125.0000 377.5000 190.0000]'
+%                              Type: 'interleaved_ascending': Default for Philips and GE
+%                                    'interleaved_ascending_siemens': Default for Siemens
+%                                    'interleaved_descending'
+%                                    'ascending'
+%                                    'descending'
 %                          (If not specified, slice timing is obtained by reading dicom header file)
 %
 % :Output(PREPROC):
@@ -101,25 +107,54 @@ for subj_i = 1:numel(preproc_subject_dir)
     %% 4. so: Slice order
     
     if exist('custom_slice_timing', 'var')
-        PREPROC.slice_time = custom_slice_timing(:).';
+        if isnumeric(custom_slice_timing)
+            PREPROC.slice_time = custom_slice_timing(:).';
+        elseif ischar(custom_slice_timing)
+            switch custom_slice_timing
+                case 'interleaved_ascending'
+                    PREPROC.slice_time = [1:2:num_slices 2:2:num_slices];
+                case 'interleaved_ascending_siemens'
+                    if mod(num_slices, 2) == 1 % odd slices
+                        PREPROC.slice_time = [1:2:num_slices 2:2:num_slices];
+                    elseif mod(num_slices, 2) == 0 % even slices
+                        PREPROC.slice_time = [2:2:num_slices 1:2:num_slices];
+                    end
+                case 'interleaved_descending'
+                    PREPROC.slice_time = fliplr([1:2:num_slices 2:2:num_slices]);
+                case 'ascending'
+                    PREPROC.slice_time = 1:num_slices;
+                case 'descending'
+                    PREPROC.slice_time = fliplr(1:num_slices);
+                otherwise
+                    error('Slice timing information is strange.');
+            end
+        else
+            error('Slice timing information is strange.');
+        end
+        fprintf('custom slice timing: \n');
     else
         dicomheader = load(PREPROC.dicomheader_files{1});
-        PREPROC.slice_time = dicomheader.h.MosaicRefAcqTimes(:).';
+        PREPROC.slice_time = dicomheader.h.MosaicRefAcqTimes(:).'; % milliseconds
+        fprintf('slice timing from dicom header: \n');
     end
+    disp(PREPROC.slice_time);
 
     slice_timing_job{1}.spm.temporal.st.so = PREPROC.slice_time;
     
     %% 5. refslice: Reference slice (first)
     
     if isequal(1:num_slices, sort(PREPROC.slice_time, 'ascend')) % Order-based
-        [~, slice_timing_job{1}.spm.temporal.st.refslice] = min(PREPROC.slice_time);
+        [~, ref_slice] = min(PREPROC.slice_time);
+        fprintf('Reference slice number: %d \n', ref_slice);
     else
-        if min(PREPROC.slice_time) >= 0 && max(PREPROC.slice_time) <= tr % Time-based
-            slice_timing_job{1}.spm.temporal.st.refslice = min(PREPROC.slice_time);
+        if min(PREPROC.slice_time) >= 0 && max(PREPROC.slice_time)/1000 <= tr % Time-based
+            ref_slice = min(PREPROC.slice_time);
+            fprintf('Reference time: %f \n', ref_slice);
         else
             error('Slice timing information is strange.');
         end
     end
+    slice_timing_job{1}.spm.temporal.st.refslice = ref_slice;
     
     %% 6. prefix
     
